3047fb3593a72a51ff3278949d1c55324eb843f1
Greg Doud <gpdoud@gmail.com>
2020-09-23 14:16:14 -0400

    Add project files.

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorldProject {
+    /// <summary>
+    /// This is the program class
+    /// This is another line of the program class
+    /// documentation
+    /// </summary>
+    class Program {
+        static void Main(string[] args) {
+            // print to console
+            Console.WriteLine("Hello World!");
+            var name = "Elena";
+            Console.WriteLine($"Hello to {name}");
+
+            var sql = "c:\\repos\\my-sql-scrips\\SalesDb-Script.sql";
+            var sqlv = @"c:\repos\my-sql-scrips\SalesDb-Script.sql";
+        }
+    }
+}
